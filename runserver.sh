98be430c7a44364829a9dc6012289bbd121f7565
unknown <ankit11hab@gmail.com>
2022-09-25 20:43:50 +0530

    gunicorn

--- runserver.sh
+++ runserver.sh
@@ -3,4 +3,4 @@ python manage.py collectstatic --no-input --clear
 #python manage.py createsuperuser --noinput --username alchercaadmin --email alcher@gmail.com
 python manage.py migrate
 # python manage.py createsuperuser --noinput --firstname admin --email admin@admin.com
-gunicorn ca_portal.wsgi:application --bind 0.0.0.0:80 --log-level=debug --timeout 180  --workers 4
+gunicorn ce_portal.wsgi:application --bind 0.0.0.0:80 --log-level=debug --timeout 180  --workers 4
