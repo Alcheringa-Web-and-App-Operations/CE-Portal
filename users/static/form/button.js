9fa781df7523fff749422f0abbd09f19da95e0a0
Akshat2403 <95174507+Akshat2403@users.noreply.github.com>
2022-09-26 18:45:33 +0530

    updated form

--- users/static/form/button.js
+++ users/static/form/button.js
@@ -6,20 +6,20 @@ var i=1
 add.addEventListener('click',target=>{
     target.preventDefault()
     form.insertAdjacentHTML('beforeend',`<div style="font-size:25px;padding-left: 40px;display: grid;grid-template-columns: 200px 0px;" id = "${i}">
-    <h3>Candidate ${i+1}<button onclick= del(${i})>Remove</button></h3>
+    <h3 id="h${i}">Candidate ${i+1}<button onclick= remove(${i})>Remove</button></h3>
     <p>
     <div>
         <label for="id_name">Name:</label>
     </div>
     <div>
-        <input type="text" name="name${i}" maxlength="50" required id="id_name">
+        <input type="text" name="name${i}" maxlength="50" id="name${i}" required id="id_name">
     </div>
     <div>
         <label for="id_name">Contact No:</label>
 
     </div>
     <div>
-        <input type="text" name="phoneNo${i}" maxlength="50" required id="id_name">
+        <input type="text" name="phoneNo${i}" maxlength="50"  id="phoneNo${i}" required id="id_name">
 
     </div>
     <div>
@@ -27,7 +27,7 @@ add.addEventListener('click',target=>{
 
     </div>
     <div>
-        <input type="email" name="email${i}" maxlength="254" required id="id_email">
+        <input type="email" name="email${i}" id="email${i}" maxlength="254" required id="id_email">
 
     </div>
     </p>
@@ -39,4 +39,18 @@ add.addEventListener('click',target=>{
 function del(div){
     document.getElementById(div).remove()
     document.getElementById("hidden").insertAdjacentHTML('beforeend',`<input type="hidden" name="name${div}" value=""/>`)
+}
+function remove(div){
+    del(div)
+    for(let j=div+1;j<i;j++){
+        document.getElementById(j).setAttribute("id",j-1)
+        document.getElementById("h"+j).innerHTML=`Candidate ${j} <button onclick= remove(${j-1})>Remove</button>`;
+        document.getElementById("h"+j).setAttribute("id",j-1)
+        document.getElementById("name"+j).setAttribute("id",j-1)
+        document.getElementById("phoneNo"+j).setAttribute("id",j-1)
+        document.getElementById("email"+j).setAttribute("id",j-1)
+    }
+    i--
+    count.value = i
+    
 }
