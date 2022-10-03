7e84d8fe64362225ac7e033e8293b972379d6b5b
unknown <ankit11hab@gmail.com>
2022-10-03 17:55:41 +0530

    minio

--- ce_portal/settings.py
+++ ce_portal/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 DEFAULT_FILE_STORAGE = "minio_storage.storage.MinioMediaStorage"
