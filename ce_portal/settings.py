8fd9c2ecb385562a4b14ceeadb86451d9bcda7f6
unknown <ankit11hab@gmail.com>
2022-10-04 00:18:50 +0530

    db

--- ce_portal/settings.py
+++ ce_portal/settings.py
@@ -79,8 +79,20 @@ WSGI_APPLICATION = 'ce_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+       'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': os.environ.get('db_name'),
+
+        'USER': os.environ.get('db_user'),
+
+        'PASSWORD': os.environ.get('db_password'),
+
+        'HOST': os.environ.get('db_host'),
+
+        'PORT': os.environ.get('db_port')
     }
 }
 
