21bcdbc481532e4d33b945900fe5c4182e2176b1
Kruthikesh Kopparam <kruthikeshm@gmail.com>
2022-10-19 17:10:44 +0530

    favicon

--- ce_portal/settings.py
+++ ce_portal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bjh9$g-x*th^276=#lijgou80br!*v72d3a+3(rzh-m60bwa%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
